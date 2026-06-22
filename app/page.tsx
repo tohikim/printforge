794e6eb4f7f4a3c3882426de3c19e5c06cfebe24
Tohi Kim <tohikimbusiness@gmail.com>
2026-06-22 17:42:48 +0400

    feat: built the printforge homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,51 @@
+import cover from "../public/home-image.png";
+import logoMobile from "../public/logoMobile.png";
+import logoPC from "../public/logoPC.png";
 import Image from "next/image";
+
+/**
+ * Challenge: Create the About page. This page will be
+ * accessible at the `/about` route. (e.g. printforge.com/about)
+ */
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="flex flex-col gap-5 justify-around align-center w-screen h-screen p-7 md:p-10">
+      <header className="flex flex-row justify-between items-center w-full mb-5">
+        <div className="block md:hidden">
+          <Image src={logoMobile} height={40} alt="Logo mobile" />
+        </div>
+        <div className="hidden md:block">
+          <Image src={logoPC} height={40} alt="Logo PC" />
+        </div>
+        {/*replace below with the nav componenet later */}
+        <div className="flex flex-row gap-5 justify-around max-w-[50%]">
+          <p>3D MODELS</p>
+          <p>ABOUT</p>
+        </div>
+      </header>
+      <div className="flex flex-col md:flex-row md:justify-around items-center gap-20 h-full">
+        <div className="flex flex-col gap-5 justify-center items-left md:max-w-[38%] ">
+          <div className="hidden md:block">
+            <p>Your go-to platform for 3D printing files</p>
+          </div>
+          <h1 className="font-bold text-4xl">
+            Discover what's possible with 3D printing
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="text-xl">
+            Join our community of creators and explore a vast library of
+            user-submitted models.
           </p>
+          {/* add onClick logic later*/}
+          <button className="mt-7 text-xl border-2 border-black w-fit px-5 py-2 bg-white opacity-100">
+            BROWSE MODELS
+          </button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex justify-center items-center md:h-fit md:max-w-[40%]">
+          <Image src={cover} alt="cover image" />
         </div>
-      </main>
+      </div>
+      <footer></footer>
     </div>
   );
 }
---
+++ app/products/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductPage() {
+  return <h1>This is the product page</h1>;
+}
