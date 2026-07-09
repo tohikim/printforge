53d0f73299b03cd24dacc639897b2bb3c6778d57
Tohi Kim <tohikimbusiness@gmail.com>
2026-07-09 18:50:19 +0500

    fix: removed output schema validation temporarily

--- app/3d-models/categories/[slug]/page.tsx
+++ app/3d-models/categories/[slug]/page.tsx
@@ -1,9 +1,7 @@
 import { CategoryPageProps, Model } from "@/app/types";
 import ModelsGrid from "../../components/ModelsGrid";
 import { getAllModels } from "@/app/lib/models";
-export default async function CategoryPage({
-  params,
-}: CategoryPageProps): Promise<JSX.Element> {
+export default async function CategoryPage({ params }: CategoryPageProps) {
   const { slug } = await params;
   const models: Model[] = await getAllModels();
   const filteredModels = models.filter(
